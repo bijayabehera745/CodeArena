1766dbaee4e949a5c939cd602fb740255a6702a7
Bijay <bijayabehera745@kgpian.iitkgp.ac.in>
2026-01-11 21:28:06 +0530

    Updates models to use MongoDB default id

--- codearena/problems/models.py
+++ codearena/problems/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from djongo import models
 
 
 class Problem(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
     sample_input = models.TextField()
@@ -11,6 +13,7 @@ class Problem(models.Model):
         ('Medium', 'Medium'),
         ('Hard', 'Hard')
     ])
+    # For test case checking internally
     test_input = models.TextField()      # hidden input
     expected_output = models.TextField()  # hidden output
 
